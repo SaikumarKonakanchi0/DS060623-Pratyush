20951422e82ed746ba611168d8971bf9e4a9a7f4
Pratyush Srivastava <63108370+pratyushsrivastava500@users.noreply.github.com>
2023-06-20 21:38:19 +0530

    Add files via upload

---
+++ Conditional Statements/Problem4.py
@@ -0,0 +1,7 @@
+# Python Program to Check weather a candidate Eligible for Vote or not
+
+age = int(input("Enter Age :"))
+if age>=18:
+    print("Candidate is Eligible for Vote")
+else:
+    print("Candidate is not Eligible for Vote")
