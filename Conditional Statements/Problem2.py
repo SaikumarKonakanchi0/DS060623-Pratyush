1720b67e261388d110c8a792bb224caab3f076e8
Pratyush Srivastava <63108370+pratyushsrivastava500@users.noreply.github.com>
2023-06-21 21:34:49 +0530

    Delete Conditional Statements directory

--- Conditional Statements/Problem2.py
+++
@@ -1,9 +0,0 @@
-day = input("Enter Day :")
-if day=="Sunday":
-    print("Hurrah Today is Sunday I will go to Play Cricket")
-elif day=="Monday":
-    print("Ohh I need to go to School")
-elif day=="Saturday":
-    print("Hurrah Tomorrow is Holiday")
-else:
-    print("OH I need to do my studies")
--- Conditional Statements/Problem6.py
+++
@@ -1,7 +0,0 @@
-# Python program to check weather Given Number is Divisible by 7 or Not
-
-num = int(input("Enter a Number :"))
-if num%7==0:
-    print("Number is Divisible by 7")
-else:
-    print("Number is not Divisble by 7")
